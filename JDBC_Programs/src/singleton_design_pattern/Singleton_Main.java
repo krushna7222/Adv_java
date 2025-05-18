d316e74e3c98bbd94cb616d7a975b419ac8ec95c
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-05-18 22:52:31 +0530

    Create Singleton_Main.java

---
+++ JDBC_Programs/src/singleton_design_pattern/Singleton_Main.java
@@ -0,0 +1,16 @@
+package singleton_design_pattern;
+
+public class Singleton_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Student1 s1 = Student1.getSingltonClass();
+		
+		
+		s1.display();
+
+	}
+
+}

7c917bca37fd753f93932f9c9fd36beaf1b467b4
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-05-23 23:08:08 +0530

    Create Sub_Servlet.java

---
+++ Mathematics_Operations/src/main/java/mathematics_operations/Sub_Servlet.java
@@ -0,0 +1,26 @@
+package mathematics_operations;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Sub_Servlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+
+		int c = a-b;
+		
+		PrintWriter out = res.getWriter();
+		
+		out.print("<h1><i>Subtraction Is : "+c+"</i></h1>");
+		}
+	
+}

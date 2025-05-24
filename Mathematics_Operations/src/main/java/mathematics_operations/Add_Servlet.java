ede2b1c7ff806a5bbf603a9a5b0ef4fe39620c37
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-05-24 23:59:28 +0530

    Create Add_Servlet.java

---
+++ Mathematics_Operations/src/main/java/mathematics_operations/Add_Servlet.java
@@ -0,0 +1,27 @@
+package mathematics_operations;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class Add_Servlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+
+		int c = a+b;
+		
+		PrintWriter out = res.getWriter();
+		
+		out.print("<h1><i>Addition Is : "+c+"</i></h1>");
+	}
+	
+}

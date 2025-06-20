c6200ff0dba8734b1ae61d29bbc6714cc6369704
Krushna Shahane <142483530+krushna7222@users.noreply.github.com>
2025-06-20 23:43:19 +0530

    Create Conn.java

---
+++ ATM_Simulator_System_master/src/ASimulatorSystem/Conn.java
@@ -0,0 +1,19 @@
+package ASimulatorSystem;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankdatabase", "root", "Patil@2003");
+            s = c.createStatement();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
